2746404ae61660c6fecbae65399de5a7fcfd77b4
VincenzoImp <vincenzoimperati99@gmail.com>
2025-09-08 19:27:31 +0200

    removed client folder

--- nostr_tools/client/__init__.py
+++
@@ -1,6 +0,0 @@
-"""WebSocket client functionality for Nostr relays."""
-
-from .websocket_client import NostrWebSocketClient
-from .event_fetcher import fetch_events
-
-__all__ = ["NostrWebSocketClient", "fetch_events"]
