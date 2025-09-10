38027eda6fd11f7093fa047990c8fddf61a1a254
VincenzoImp <vincenzoimperati99@gmail.com>
2025-09-10 14:58:15 +0200

    updated init file

--- nostr_tools/actions/__init__.py
+++ nostr_tools/actions/__init__.py
@@ -4,10 +4,20 @@ from .actions import (
     fetch_events,
     stream_events,
     fetch_nip11,
+    check_connectivity,
+    check_readability,
+    check_writability,
+    fetch_connection,
+    compute_relay_metadata,
 )
 
 __all__ = [
     "fetch_events",
     "stream_events",
     "fetch_nip11",
+    "check_connectivity",
+    "check_readability",
+    "check_writability",
+    "fetch_connection",
+    "compute_relay_metadata",
 ]
